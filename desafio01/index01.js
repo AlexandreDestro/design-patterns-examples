c205c8b6a398f8a3ba81ebc5fb87353a64ce60e1
Alexandre Destro Zanoni <alexandre.68314@alunosatc.edu.br>
2025-09-18 19:13:02 -0300

    a

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,55 @@
-console.log("Hello, World! 001");
+// Produtos
+class LightButton {
+    render() {
+      return "Botão branco criado";
+    }
+  }
+  class DarkButton {
+    render() {
+      return "Botão preto criado";
+    }
+  }
+  
+  class LightWindow {
+    render() {
+      return "Janela clara aberta";
+    }
+  }
+  class DarkWindow {
+    render() {
+      return "Janela escura aberta";
+    }
+  }
+  
+
+  class LightThemeFactory {
+    createButton() {
+      return new LightButton();
+    }
+    createWindow() {
+      return new LightWindow();
+    }
+  }
+  
+  class DarkThemeFactory {
+    createButton() {
+      return new DarkButton();
+    }
+    createWindow() {
+      return new DarkWindow();
+    }
+  }
+  
+
+  const theme = "dark"; 
+  
+
+  const factory = theme === "light" ? new LightThemeFactory() : new DarkThemeFactory();
+  
+
+  const button = factory.createButton();
+  const windowUI = factory.createWindow();
+  
+  console.log(button.render());
+  console.log(windowUI.render());
+  
